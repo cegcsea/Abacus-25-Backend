ed00222cd11a8798bceb265015eef50334f890ab
Selva Rathinam  M <selva152004@gmail.com>
2025-02-09 13:09:58 +0530

    Final Changes

--- index.js
+++ index.js
@@ -18,13 +18,7 @@ const prisma = new PrismaClient();
 // Serve the images folder as a static directory
 app.use("/images", express.static(path.join(__dirname, "images")));
 
-app.use(
-  cors({
-    origin: ["http://localhost:3000", "http://localhost:3002"],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  })
-);
+app.use(cors());
 
 app.use(express.json());
 app.use(morgan("dev"));
