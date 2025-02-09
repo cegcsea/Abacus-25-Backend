911225519813f7dadbf47bf0f3c549432fec6bdc
nikhil03a <anikhil03prasanna@gmail.com>
2025-02-10 00:16:56 +0530

    ss upload fix

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -1,8 +1,10 @@
 import multer from 'multer';
 import path from 'path';
+import { fileURLToPath } from "url";
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'images/');
+    cb(null, path.join(__dirname, '..' , 'images/'));
   },
   filename: (req, file, cb) => {
     cb(null, req.params.workshopPaymentId + '_' + Date.now() + path.extname(file.originalname));
