a0fe4850a4badff46705312e76d2fedbe68c0e42
n-kamalesh <kamaltvt579@gmail.com>
2026-02-13 21:14:06 +0530

    Refactor upload middleware to ensure upload directory exists and improve code consistency

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -1,14 +1,27 @@
-import multer from 'multer';
-import path from 'path';
+import multer from "multer";
+import path from "path";
+import fs from "fs";
 import { fileURLToPath } from "url";
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const uploadDir = path.join(__dirname, "..", "images/");
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir, { recursive: true });
+}
+
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, path.join(__dirname, '..' , 'images/'));
+    cb(null, uploadDir);
   },
   filename: (req, file, cb) => {
-    cb(null, req.params.workshopPaymentId + '_' + Date.now() + path.extname(file.originalname));
-  }
+    cb(
+      null,
+      req.params.workshopPaymentId +
+        "_" +
+        Date.now() +
+        path.extname(file.originalname),
+    );
+  },
 });
 const upload = multer({ storage });
 
